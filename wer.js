38d71a40f603a7efab75bdde6c9e34b7e7d9de75
YoussefNsserALlam <128973505+YoussefNsserALlam@users.noreply.github.com>
2023-07-10 20:00:49 +0300

    Update wer.js

--- wer.js
+++ wer.js
@@ -1,15 +1,1 @@
 
-const swiper = new Swiper('.swiper', {
-
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
